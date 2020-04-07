117796430fd23da083b6bf27d34bfa5ba3b15d37
Alexey Bulygin <mdss@makexhtml.ru>
2020-04-07 22:01:25 +0300

    First task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let total = 1;
+  for (let i = 0; i < n; i++) {
+    total = total * (n - i);
+  }
+  return total;
 }
+factorial(3);
