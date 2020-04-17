55b8f518eb41ced9e3f41456da3a360b801ee525
Alexey Bulygin <mdss@makexhtml.ru>
2020-04-17 15:43:31 +0300

    done makeFriendsList function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -7,7 +7,7 @@
   let ul = document.createElement('ul');
   for (let i = 0; i < friends.length; i ++) {
     let li = document.createElement('li');
- 		li.innerHTML = `${friends[i].firstName} + ${friends[i].lastName}`;
+ 		li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
  		ul.appendChild(li);
  	}
  	return ul;
