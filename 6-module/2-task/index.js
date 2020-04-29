6aff54b0f88384032f4702aa42c11518fcd19b7d
Alexey Bulygin <mdss@makexhtml.ru>
2020-04-29 13:31:33 +0300

    refactoring ProductCard class

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -3,20 +3,22 @@ import createElement from '../../assets/lib/create-element.js';
 export default class ProductCard {
   constructor(product) {
     this.elem = document.createElement('div');
-    this.elem.addEventListener('click', (event) => this.productAdd(event));
+
+    // add card class for the root element
+    this.elem.classList.add('card');
+
+    this.elem.addEventListener('click', (event) => this.addToCart(event));
+
     this.id = product.id;
+
     this.render(product);
-    return this.elem.innerHTML;
   }
 
   render(product) {
     // set product pictures path
     let productImagePath = '/assets/images/products';
 
-    // add card class for the root element
-    this.elem.classList.add('card');
-
-    // create product card
+    // create product card skeleton
     this.elem.innerHTML = `
     <div class="card__top">
       <img src="${productImagePath}/${product.image}" class="card__image" alt="product">
@@ -29,10 +31,9 @@ export default class ProductCard {
       </button>
     </div>
     `;
-    return this.elem;
   }
 
-  productAdd(event) {
+  addToCart(event) {
     if ( event.target.closest('.card__button') ) {
       this.elem.dispatchEvent(new CustomEvent("product-add", {
         detail: this.id,
