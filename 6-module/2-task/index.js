f4d6bdfcf9cd666305977a486fad1dd839f6a2fd
Alexey Bulygin <mdss@makexhtml.ru>
2020-04-29 16:25:40 +0300

    transfer create elem and add class to render method

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,19 +2,19 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
-    this.elem = document.createElement('div');
-
-    // add card class for the root element
-    this.elem.classList.add('card');
-
     this.elem.addEventListener('click', (event) => this.addToCart(event));
-
     this.id = product.id;
 
     this.render(product);
   }
 
   render(product) {
+    // create element
+    this.elem = document.createElement('div');
+
+    // add card class for the root element
+    this.elem.classList.add('card');
+    
     // set product pictures path
     let productImagePath = '/assets/images/products';
 
