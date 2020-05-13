3c9d6292d0f3d3fb1e301a6e0327a3cc6b43ab64
Alexey Bulygin <mdss@makexhtml.ru>
2020-05-13 18:19:21 +0300

    fix bounding rect

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,31 +39,32 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    if (this.elem.closest('.cart-icon_visible')) {
-      if ( document.documentElement.clientWidth <= 767 ) {
 
-        // clear all elem styles
-        this.elem.style.position = '';
-        this.elem.style.top = '';
-        this.elem.style.left = '';
-        this.elem.style.zIndex = '';
+    if ( document.documentElement.clientWidth <= 767 ) {
 
+      // clear all elem styles
+      this.elem.style.position = '';
+      this.elem.style.top = '';
+      this.elem.style.left = '';
+      this.elem.style.zIndex = '';
+
+    } else {
+
+      this.elem.style.position = 'fixed';
+      this.elem.style.top = `50px`;
+      this.elem.style.zIndex = '10000';
+
+      // calculate right position
+      let container = document.querySelector('.container');
+      let rightSpace = document.documentElement.clientWidth - container.getBoundingClientRect().left - container.offsetWidth;
+      if ( rightSpace >= this.elem.offsetWidth + 20 ) {
+        this.elem.style.left = container.getBoundingClientRect().left + container.offsetWidth + 20 + 'px';
       } else {
-
-        this.elem.style.position = 'fixed';
-        this.elem.style.top = `50px`;
-        this.elem.style.zIndex = '10000';
-
-        // calculate right position
-        let container = document.querySelectorAll('.container');
-        let rightSpace = document.documentElement.clientWidth - container[0].getBoundingClientRect().left - container[0].offsetWidth;
-        if ( rightSpace >= this.elem.offsetWidth + 20 ) {
-          this.elem.style.left = container[0].getBoundingClientRect().left + container[0].offsetWidth + 20 + 'px';
-        } else {
-          this.elem.style.left = document.documentElement.clientWidth - this.elem.offsetWidth - 10 + 'px';
-        }
+        this.elem.style.left = document.documentElement.clientWidth - this.elem.offsetWidth - 10 + 'px';
       }
+
     }
 
   }
+
 }
