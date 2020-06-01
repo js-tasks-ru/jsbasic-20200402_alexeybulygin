7eb9cbe19fdb451eae4c94aca924977338dc88bb
Alexey Bulygin <mdss@makexhtml.ru>
2020-06-01 18:17:34 +0300

    fix cartIcon position

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -4,17 +4,18 @@ export default class CartIcon {
   constructor() {
     this.render();
     this.addEventListeners();
+
   }
 
   render() {
     this.elem = createElement('<button class="cart-icon"></button>');
-    this.elem.topCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+
   }
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
 
+      this.elem.classList.add('cart-icon_visible');
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
@@ -22,6 +23,7 @@ export default class CartIcon {
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
 
+      this.topCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
       this.updatePosition();
 
       this.elem.classList.add('shake');
@@ -32,6 +34,7 @@ export default class CartIcon {
     } else {
       this.elem.classList.remove('cart-icon_visible');
     }
+
   }
 
   addEventListeners() {
@@ -43,7 +46,7 @@ export default class CartIcon {
 
     if (!this.elem.offsetHeight) {return;} // not visible, for mocha test
 
-    if ( window.pageYOffset > this.elem.topCoord && document.documentElement.clientWidth >= 767 ) {
+    if ( document.documentElement.clientWidth >= 767 ) {
 
       this.elem.style.position = 'fixed';
       this.elem.style.top = `50px`;
@@ -51,11 +54,14 @@ export default class CartIcon {
 
       // calculate right position
       let container = document.querySelector('.container');
-      let rightSpace = document.documentElement.clientWidth - container.getBoundingClientRect().left - container.offsetWidth;
-      if ( rightSpace >= this.elem.offsetWidth + 20 ) {
+      let rightSpace = document.documentElement.offsetWidth - container.getBoundingClientRect().left - container.offsetWidth;
+      if ( window.pageYOffset > this.topCoord && rightSpace >= this.elem.offsetWidth + 20 ) {
         this.elem.style.left = container.getBoundingClientRect().left + container.offsetWidth + 20 + 'px';
+      } else if ( window.pageYOffset < this.topCoord && rightSpace >= this.elem.offsetWidth + 20 ) {
+        this.elem.style.position = '';
+        this.elem.style.left = '';
       } else {
-        this.elem.style.left = document.documentElement.clientWidth - this.elem.offsetWidth - 10 + 'px';
+        this.elem.style.left = document.documentElement.offsetWidth - this.elem.offsetWidth - 10 + 'px';
       }
 
     } else {
@@ -66,7 +72,6 @@ export default class CartIcon {
       this.elem.style.zIndex = '';
 
     }
-
 
   }
 
